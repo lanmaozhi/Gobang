6dfbbacbb512da0da5537e59c7509a97973fa610
lanmz <blue_163y@163.com>
2020-08-07 23:22:26 +0800

    棋盘初步提交

--- src/main/java/chessboard/GobangChessBoard.java
+++ src/main/java/chessboard/GobangChessBoard.java
@@ -7,6 +7,8 @@ import static utils.Constants.ROW_NUM;
 import static utils.Constants.COLUMN_NUM;
 import static utils.Constants.CROSS_NUM;
 import static utils.Constants.MASK_CODES;
+import static utils.Constants.LEFT_CROSS_PLACE;
+import static utils.Constants.RIGHT_CROSS_PLACE;
 
 public class GobangChessBoard implements Chessboard {
 
@@ -32,8 +34,34 @@ public class GobangChessBoard implements Chessboard {
         return winSide;
     }
 
-    private void judgeIsComplete() {
+    private void judgeIsComplete(ChessTypeEnum chessType) {
 
+        if (judgeHasFiveBits(rows[chessType.index]) || judgeHasFiveBits(columns[chessType.index])
+                || judgeHasFiveBits(leftCrosses[chessType.index]) || judgeHasFiveBits(rightCrosses[chessType.index])) {
+            winSide = chessType;
+        }
+    }
+
+    private boolean judgeHasFiveBits(int[] ints) {
+
+        //15列，所以用掩码比较11次即可
+        int countTimes = 11;
+        //掩码是 11111000000000000000000000000000
+        int mask = 0xF8000000;
+        //开始比较
+        for (int anInt : ints) {
+            if (anInt == 0) {
+                continue;
+            }
+            for (int i = 0; i < countTimes; i++) {
+                int nMask = mask >>> i;
+                if ((anInt & nMask) == nMask) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     @Override
@@ -56,7 +84,7 @@ public class GobangChessBoard implements Chessboard {
         makeAMoveToBitMap(chessType, x, y);
 
         //计算是否胜利
-        judgeIsComplete();
+        judgeIsComplete(chessType);
 
         return true;
     }
@@ -68,10 +96,10 @@ public class GobangChessBoard implements Chessboard {
         rows[chessType.index][x] |= MASK_CODES[y];
         //列
         columns[chessType.index][y] |= MASK_CODES[x];
-        //左斜对角线
-
-        //右斜对角线
-
+        //左下-右上
+        leftCrosses[chessType.index][LEFT_CROSS_PLACE[x][y][0]] |= MASK_CODES[LEFT_CROSS_PLACE[x][y][1]];
+        //右上-左下
+        rightCrosses[chessType.index][RIGHT_CROSS_PLACE[x][y][0]] |= MASK_CODES[RIGHT_CROSS_PLACE[x][y][1]];
     }
 
     //判断位置(x,y)上是否有棋子
@@ -79,5 +107,24 @@ public class GobangChessBoard implements Chessboard {
 
         return (rows[ChessTypeEnum.BLACK.index][x] & MASK_CODES[y]) != 0
                 || (rows[ChessTypeEnum.WHITE.index][x] & MASK_CODES[y]) != 0;
+    }
+
+    public static void main(String[] args) {
+
+        GobangChessBoard board1 = new GobangChessBoard();
+        board1.makeAMove(ChessTypeEnum.BLACK, 1, 14);
+        board1.makeAMove(ChessTypeEnum.BLACK, 1, 13);
+        board1.makeAMove(ChessTypeEnum.BLACK, 1, 12);
+        board1.makeAMove(ChessTypeEnum.BLACK, 1, 11);
+        board1.makeAMove(ChessTypeEnum.BLACK, 1, 10);
+
+        GobangChessBoard board2 = new GobangChessBoard();
+        board2.makeAMove(ChessTypeEnum.WHITE, 1, 1);
+        board2.makeAMove(ChessTypeEnum.WHITE, 2, 2);
+        board2.makeAMove(ChessTypeEnum.WHITE, 3, 3);
+        board2.makeAMove(ChessTypeEnum.WHITE, 4, 4);
+        board2.makeAMove(ChessTypeEnum.WHITE, 5, 5);
+
+        return;
     }
 }
